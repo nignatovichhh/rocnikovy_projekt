fbedb777b796871b3f1fda98fdde2fb564106623
Anastasiya <anastasiyaihnatovich@gmail.com>
2025-01-20 22:08:03 +0100

    Deleted debug info

--- apis/openaiapi.py
+++ apis/openaiapi.py
@@ -9,16 +9,20 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 class OpenAIApiWrapper():
     def __init__(self):
+        # Client initialization
         self.client = OpenAI(
              api_key=OPENAI_API_KEY,  
         )
+
+        # Reading prompt from file
         with open('./apis/context_prompt.txt', 'r', encoding='utf-8') as file:
             self.context_prompt = file.read()
 
     def send_request(self,query):
         query = "Input JSON: " + query
         query += self.context_prompt
-        print(query)
+
+        # Sending prompt to OpenAI API
         chat_completion = self.client.chat.completions.create(
         messages=[
                 {
@@ -28,16 +32,13 @@ class OpenAIApiWrapper():
             ],
             model="gpt-4o",
         ) 
-        print(chat_completion)
 
+        # Retrieving response
         lines = chat_completion.choices[0].message.content.split('\n')
-        print(lines)
         trimmed_lines = "\n".join(lines[1:-1])
-        print(trimmed_lines)
 
         try:
             result_json = json.loads(trimmed_lines)
-            print(result_json)
             return result_json
         except json.JSONDecodeError as e:
             print("Failed to decode JSON:", e)
@@ -45,6 +46,7 @@ class OpenAIApiWrapper():
         return None
 
 if __name__ == "__main__":
+    # EXAMPLE 
     wrapper = OpenAIApiWrapper()
     query = '{ "stcity: "Bratislava", "destcity": "Linz", "from": "2025-01-19T09:00", "to": "2025-01-19T19:00", "interests": ["culture", "nature","active leisure"], "numpeop": 2, "budget": 100, "age": ["18-30"]}'
     print(wrapper.send_request(query))
--- apis/tripadvisorapi.py
+++ apis/tripadvisorapi.py
@@ -9,8 +9,6 @@ TA_API_KEY = os.getenv("TA_API_KEY")
 
 class TripAdvisorApiWrapper():
     def __init__(self):
-        print("KEY")
-        print(TA_API_KEY)
         self.api_key = TA_API_KEY
 
     def send_request(self, url: str):
@@ -20,19 +18,38 @@ class TripAdvisorApiWrapper():
         return json.loads(response.text)
     
     def find_search(self, search_query: str, lat_long: str, radius: int):
+        '''
+            Parameters: 
+                search_query - location name; 
+                lat_long - approx. latitude, longitude;
+                radius - radius for search with center in lat_long
+            Used for:
+                retrieving location id and address
+        '''
         url = f'https://api.content.tripadvisor.com/api/v1/location/search?language=en&key={self.api_key}'
         url += f"&searchQuery={search_query}"
         url += f"&latLong={lat_long}"
         url += f"&radius={radius}"
         url += f"&radiusUnit=m"
-        print(url)
         return self.send_request(url)
 
     def location_photos(self,location_id: str):
+        '''
+            Parameters: 
+                location_id - id from TripAdvisor retrieved using fin_search; 
+            Used for:
+                retrieving photos
+        '''
         url = f"https://api.content.tripadvisor.com/api/v1/location/{location_id}/photos?language=en&key={self.api_key}"
         return self.send_request(url)
 
     def location_details(self,location_id: str):
+        '''
+            Parameters: 
+                location_id - id from TripAdvisor retrieved using fin_search; 
+            Used for:
+                retrieving latitude, longitude
+        '''
         url = location_details_url = f"https://api.content.tripadvisor.com/api/v1/location/{location_id}/details?language=en&currency=USD&key={self.api_key}"
         return self.send_request(url)
 
